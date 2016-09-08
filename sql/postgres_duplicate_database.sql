2e48e477d171a28fc5c2ceba1ee99e2f0d18a180
Marc Streit <marc.streit@jku.at>
2016-09-08 16:47:26 +0200

    Adding sql commands that reduces tables to a size of 100

--- sql/postgres_duplicate_database.sql
+++ sql/postgres_duplicate_database.sql
@@ -4,3 +4,30 @@ WHERE pg_stat_activity.datname = 'postgres' AND pid <> pg_backend_pid();
 
 /* CLONE DATABASE TO NEW ONE(TARGET_DB) */
 CREATE DATABASE postgres-tiny WITH TEMPLATE postgres OWNER postgres;
+
+DELETE FROM cellline.targid_cellline
+WHERE celllinename NOT IN (SELECT celllinename FROM cellline.targid_cellline ORDER BY celllinename LIMIT 100);
+
+DELETE FROM cellline.targid_copynumber
+WHERE ensg NOT IN (SELECT ensg FROM cellline.targid_copynumber ORDER BY ensg LIMIT 100);
+
+DELETE FROM cellline.targid_expression
+WHERE ensg NOT IN (SELECT ensg FROM cellline.targid_expression ORDER BY ensg LIMIT 100);
+
+DELETE FROM cellline.targid_mutation
+WHERE ensg NOT IN (SELECT ensg FROM cellline.targid_mutation ORDER BY ensg LIMIT 100);
+
+DELETE FROM tissue.targid_tissue
+WHERE tissuename NOT IN (SELECT tissuename FROM tissue.targid_tissue ORDER BY tissuename LIMIT 100);
+
+DELETE FROM tissue.targid_copynumber
+WHERE ensg NOT IN (SELECT ensg FROM tissue.targid_tissue ORDER BY ensg LIMIT 100);
+
+DELETE FROM tissue.targid_expression
+WHERE ensg NOT IN (SELECT ensg FROM tissue.targid_tissue ORDER BY ensg LIMIT 100);
+
+DELETE FROM tissue.targid_mutation
+WHERE ensg NOT IN (SELECT ensg FROM tissue.targid_tissue ORDER BY ensg LIMIT 100);
+
+DELETE FROM public.targid_gene
+WHERE ensg NOT IN (SELECT ensg FROM public.targid_gene ORDER BY ensg LIMIT 100);
