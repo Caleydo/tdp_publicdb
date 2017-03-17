097c5e0961d97390820412b90cc074bcfd4be05b
Christian Lehner <christian.lehner@datavisyn.io>
2017-03-17 14:33:10 +0100

    closes Caleydo/targid_common#18

--- phovea.js
+++ phovea.js
@@ -331,6 +331,17 @@ module.exports = function(registry) {
     'idtype': 'Tissue',
     'selection': 'multiple'
   });
+
+  registry.push('targidView', 'pubmed', function () {
+    return System.import('./src/views/GeneSymbolProxyView');
+  }, {
+    'name': 'PubMed',
+    'category': 'static',
+    'site': '//www.ncbi.nlm.nih.gov/pubmed?term={gene}',
+    'argument': 'gene',
+    'idtype': 'Ensembl',
+    'selection': 'multiple'
+  });
   // generator-phovea:end
 };
 
