a8d4f894351b015d5e1ac2e2cc9e851282f4db61
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-06-07 13:57:20 +0200

    use FormBuilderDialog for scores

--- src/scores/SingleScore.ts
+++ src/scores/SingleScore.ts
@@ -2,18 +2,15 @@
  * Created by sam on 06.03.2017.
  */
 
-import {generateDialog} from 'phovea_ui/src/dialogs';
 import {getAPIJSON} from 'phovea_core/src/ajax';
 import {Range, RangeLike} from 'phovea_core/src/range';
 import {IDType} from 'phovea_core/src/idtype';
-import {select} from 'd3';
 import {getSelectedSpecies} from 'targid_common/src/Common';
 import {IDataSourceConfig, gene, tissue, cellline} from '../config';
 import {convertLog2ToLinear, limitScoreRows} from '../utils';
 import {IScore} from 'ordino/src/LineUpView';
 import {createDesc} from './utils';
 import {IFormElementDesc, FormElementType} from 'ordino/src/form';
-import FormBuilder from 'ordino/src/form/FormBuilder';
 import {ParameterFormIds, FORM_GENE_NAME, FORM_TISSUE_NAME, FORM_CELLLINE_NAME} from 'targid_boehringer/src/forms';
 import {IPluginDesc} from 'phovea_core/src/plugin';
 import AScore from './AScore';
@@ -21,6 +18,7 @@ import {FORM_SINGLE_SCORE} from './forms';
 import {selectDataSources} from './utils';
 import {mixin} from 'phovea_core/src';
 import {INamedSet} from 'ordino/src/storage';
+import FormBuilderDialog from 'ordino/src/form/FormDialog';
 
 interface ISingleScoreParam {
   name: {id: string, text: string};
@@ -65,70 +63,51 @@ function enableMultiple(desc: any): any {
 
 export function create(pluginDesc: IPluginDesc) {
   const {opposite} = selectDataSources(pluginDesc);
-  // resolve promise when closing or submitting the modal dialog
-  return new Promise<ISingleScoreParam>((resolve) => {
-    const dialog = generateDialog('Add Single Score Column', 'Add Single Score Column');
+  const dialog = new FormBuilderDialog('Add Single Score Column', 'Add Single Score Column');
+  const formDesc:IFormElementDesc[] = FORM_SINGLE_SCORE.slice();
+  switch(opposite) {
+    case gene:
+      formDesc.unshift(enableMultiple(FORM_GENE_NAME));
+      break;
+    case tissue:
+      formDesc.unshift(enableMultiple(FORM_TISSUE_NAME));
+      break;
+    case cellline:
+      formDesc.unshift(enableMultiple(FORM_CELLLINE_NAME));
+      break;
+  }
+  dialog.append(...formDesc);
 
-    const form:FormBuilder = new FormBuilder(select(dialog.body));
-    const formDesc:IFormElementDesc[] = FORM_SINGLE_SCORE.slice();
+  return dialog.showAsPromise((builder) => {
+    const data = <any>builder.getElementData();
 
-    switch(opposite) {
-      case gene:
-        formDesc.unshift(enableMultiple(FORM_GENE_NAME));
-        break;
-      case tissue:
-        formDesc.unshift(enableMultiple(FORM_TISSUE_NAME));
-        break;
-      case cellline:
-        formDesc.unshift(enableMultiple(FORM_CELLLINE_NAME));
-        break;
+    {
+      const datatypes = data[ParameterFormIds.DATA_HIERARCHICAL_SUBTYPE];
+      delete data[ParameterFormIds.DATA_HIERARCHICAL_SUBTYPE];
+      const resolved = datatypes.map((entry) => entry.id.split('-'));
+      if (datatypes.length === 1) {
+        data.data_type = resolved[0][0];
+        data.data_subtype = resolved[0][1];
+      } else {
+        data.data_types = resolved;
+      }
     }
 
-    form.build(formDesc);
-
-    dialog.onSubmit(() => {
-      if (!form.validate()) {
-        return false;
-      }
-      const data = <any>form.getElementData();
-
-      {
-        const datatypes = data[ParameterFormIds.DATA_HIERARCHICAL_SUBTYPE];
-        delete data[ParameterFormIds.DATA_HIERARCHICAL_SUBTYPE];
-        const resolved = datatypes.map((entry) => entry.id.split('-'));
-        if (datatypes.length === 1) {
-          data.data_type = resolved[0][0];
-          data.data_subtype = resolved[0][1];
-        } else {
-          data.data_types = resolved;
-        }
-      }
-
-      switch (opposite) {
-        case gene:
-          data.name = data[ParameterFormIds.GENE_SYMBOL];
-          delete data[ParameterFormIds.GENE_SYMBOL];
-          break;
-        case tissue:
-          data.name = data[ParameterFormIds.TISSUE_NAME];
-          delete data[ParameterFormIds.TISSUE_NAME];
-          break;
-        case cellline:
-          data.name = data[ParameterFormIds.CELLLINE_NAME];
-          delete data[ParameterFormIds.CELLLINE_NAME];
-          break;
-      }
-
-      dialog.hide();
-      resolve(data);
-      return false;
-    });
-
-    dialog.onHide(() => {
-      dialog.destroy();
-    });
-
-    dialog.show();
+    switch (opposite) {
+      case gene:
+        data.name = data[ParameterFormIds.GENE_SYMBOL];
+        delete data[ParameterFormIds.GENE_SYMBOL];
+        break;
+      case tissue:
+        data.name = data[ParameterFormIds.TISSUE_NAME];
+        delete data[ParameterFormIds.TISSUE_NAME];
+        break;
+      case cellline:
+        data.name = data[ParameterFormIds.CELLLINE_NAME];
+        delete data[ParameterFormIds.CELLLINE_NAME];
+        break;
+    }
+    return data;
   });
 }
 
