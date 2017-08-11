f453d703ad46400de694fe8a5f5cb407e39b3809
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-08-11 10:53:21 +0200

    update update scripts

---
+++ sql/update/update_2.1.1-similaritytype.sql
@@ -0,0 +1,10 @@
+/*==============================================================*/
+/* Table: SIMILARITYTYPE                                        */
+/*==============================================================*/
+create table SIMILARITYTYPE (
+SIMILARITYTYPE       TEXT                 not null,
+SIMILARITYDESCRIPTION TEXT                 null,
+constraint PK_SIMILARITYTYPE primary key (SIMILARITYTYPE)
+);
+	  
+--empty for now
--- sql/update/update_2.2.0-tissue.sql
+++ sql/update/update_2.2.0-tissue.sql
@@ -16,9 +16,17 @@ drop table tissue.SIMILARITYTYPE;
 
 /* Table: TISSUE                                                */
 /*==============================================================*/
-alter table tissue.tissue add "sequenced" TYPE BOOL;
+alter table tissue.tissue add "DNASEQUENCED" TYPE BOOL;
+alter table tissue.tissue add "TUMORPURITY" TYPE FLOAT4;
 
 alter table tissue.tissue
 add constraint FK_TISSUE_TUMORTYPEADJACENT foreign key (TUMORTYPE_ADJACENT)
   references tissue.TUMORTYPE (TUMORTYPE)
   on delete restrict on update restrict;
+
+alter table NGSRUN
+  drop constraint FK_NGSRUN_REFERENCE_TISSUE;
+alter table NGSRUN
+   add constraint FK_NGSRUN_REFERENCE_TISSUE foreign key (TISSUENAME)
+      references TISSUE (TISSUENAME)
+      on delete cascade on update cascade;
