d44969dadd5c79c059f7a4ca67f2c31f002c3e32
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-09-04 14:52:39 +0200

    remove unused fields

--- src/forms.ts
+++ src/forms.ts
@@ -16,21 +16,14 @@ import {getTDPData, getTDPLookupUrl} from 'tdp_core/src/rest';
  * Reuse this ids and activate the `useSession` option for form elements to have the same selectedIndex between different views
  */
 export class ParameterFormIds {
-  static SPECIES = SPECIES_SESSION_KEY; // used as db field! be careful when renaming
   static DATA_SOURCE = 'data_source';
-  static FILTER_BY = 'filter_by';
   static GENE_SYMBOL = 'gene_symbol';
   static CELLLINE_NAME = 'cellline_name';
   static TISSUE_NAME = 'tissue_name';
-  static TUMOR_TYPE = 'tumor_type';
-  static TISSUE_PANEL_NAME = 'tissue_panel_name';
-  static ALTERATION_TYPE = 'alteration_type';
-  static DATA_TYPE = 'data_type';
   static DATA_SUBTYPE = 'data_subtype';
   static DATA_HIERARCHICAL_SUBTYPE = 'hierarchical_data_subtype';
   static COPYNUMBER_SUBTYPE = FORM_COPYNUMBER_SUBTYPE_ID;
   static EXPRESSION_SUBTYPE = FORM_EXPRESSION_SUBTYPE_ID;
-  static BIO_TYPE = 'bio_type';
   static AGGREGATION = 'aggregation';
   static COMPARISON_OPERATOR = 'comparison_operator';
   static COMPARISON_VALUE = 'comparison_value';
