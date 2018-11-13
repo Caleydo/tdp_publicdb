0318981b5458f028e282b5427848d22c8338d8d1
Samuel Gratzl <sam@sgratzl.com>
2018-11-13 13:02:24 +0100

    update and try to fix tour

--- src/tours/index.ts
+++ src/tours/index.ts
@@ -1,6 +1,16 @@
 import {IStep} from 'tdp_core/src/extensions';
-import {waitFor, wait} from 'tdp_core/src/tour/scripter';
-import {setValueAndTrigger} from '../../../tdp_core/src/tour/scripter';
+import {waitFor, wait, ensure} from 'tdp_core/src/tour/scripter';
+
+
+function ensureLineUpOpen() {
+  ensure(() => {
+    const openSearchBox = !document.querySelector('.modal.in');
+    const s = document.querySelector('.lu-search');
+    s.classList.toggle('lu-search-open', openSearchBox);
+    s.parentElement!.classList.toggle('once', openSearchBox);
+    return openSearchBox;
+  });
+}
 
 export function create(): IStep[] {
   return [{
@@ -16,7 +26,7 @@ export function create(): IStep[] {
     {
       selector: '.startMenu.open .speciesSelector .nav-tabs',
       html: `<p>You can choose between the three entity types <i>'Cell Lines'</i>, <i>'Genes'</i>, and <i>'Tissues'</i>.</p> <p>In this example we will work with a list of genes</p>`,
-      placement: 'right',
+      placement: 'right-start',
       postAction: () => {
         document.querySelector<HTMLElement>('#entityType_gene-entry-point').click();
       }
@@ -24,48 +34,52 @@ export function create(): IStep[] {
 
     {
       selector: '#entity_gene-entry-point .predefined-named-sets',
-      html: `Of the available predefined gene sets, we open a list of known cancer genes, called 'Cancer Gene Census'`,
-      placement: 'right',
-      waitFor: () => waitFor('.does-not-exist', Infinity).then(() => <'next'>'next')
+      html: `Of the available predefined gene sets, we open a list of known cancer genes, called <i>'Cancer Gene Census'</i>`,
+      placement: 'right-start',
+      pageBreak: true
     },
     {
       selector: '.lineup-engine.lineup-multi-engine',
       placement: 'top',
       html: `The information is presented in a tabular format. Additionally to the gene ID, a set of columns containing some basic information is shown by default.`,
-      preAction: () => waitFor('.lineup-engine.lineup-multi-engine', Infinity)
+      preAction: () => waitFor('.lineup-engine.lineup-multi-engine', Infinity).then(() => wait(500))
     },
     {
       selector: '.lu-side-panel button.fa-plus',
       html: `Additional columns can be added using the plus sign.`,
       placement: 'left',
-      postAction: () => {
-        document.querySelector<HTMLElement>('.lu-side-panel button.fa-plus').click();
-      }
+      waitFor: () => waitFor('.lu-search-open', Infinity).then(() => <'next'>'next'),
+      // postAction: () => {
+      //   document.querySelector<HTMLElement>('.lu-side-panel button.fa-plus').click();
+      // }
     },
     {
       selector: '.lu-search .lu-search-item',
       html: `First, we want to add a metadata column`,
       placement: 'left',
-      preAction: () => wait(1000),
-      postAction: () => {
-        document.querySelector<HTMLElement>('.lu-search .lu-search-item').click();
-      }
+      preAction: ensureLineUpOpen,
+      waitFor: () => waitFor('.modal.in select[name=column]', Infinity).then(() => <'next'>'next'),
+      // postAction: () => {
+      //   document.querySelector<HTMLElement>('.lu-search .lu-search-item').click();
+      // }
     },
     {
-      selector: '.modal select[name=column]',
-      html: `Here we select 'Strand' and click 'Add Column'`,
-      placement: 'left',
+      selector: '.modal.in .modal-content',
+      html: `Here we select <i>'Strand'</i> and click <i>'Add Column'</i>`,
+      placement: 'right-start',
       preAction: async () => {
-        const elem = await waitFor('.modal select[name=column]');
+        const elem = await waitFor('.modal.in select[name=column]');
+        await wait(250);
         if (elem) {
           elem.focus();
         }
       },
-      postAction: () => {
-        const s = document.querySelector<HTMLSelectElement>('.modal select[name=column]');
-        setValueAndTrigger(s, 'strand');
-        s.closest('.modal').querySelector<HTMLElement>('.modal-footer button[type=submit]').click();
-      }
+      waitFor: () => waitFor('.lineup-engine header section[title=Strand]', Infinity).then(() => <'next'>'next'),
+      // postAction: () => {
+      //   const s = document.querySelector<HTMLSelectElement>('.modal select[name=column]');
+      //   setValueAndTrigger(s, 'strand');
+      //   s.closest('.modal').querySelector<HTMLElement>('.modal-footer button[type=submit]').click();
+      // }
     },
     {
       selector: '.lineup-engine header section[title=Strand]',
@@ -76,38 +90,55 @@ export function create(): IStep[] {
       selector: '.lu-search .lu-search-group .lu-search-item',
       placement: 'left',
       html: `Now, we want to add a column containing the copy number information of two specific cell lines. To do so, click here`,
-      preAction: () => {
-        document.querySelector<HTMLElement>('.lu-side-panel button.fa-plus').click();
-      },
-      postAction: () => {
-        document.querySelector<HTMLElement>('.lu-search .lu-search-group .lu-search-item').click();
-      }
+      preAction: ensureLineUpOpen,
+      waitFor: () => waitFor('.modal.in', Infinity).then(() => <'next'>'next'),
+      // postAction: () => {
+      //   document.querySelector<HTMLElement>('.lu-search .lu-search-group .lu-search-item').click();
+      // }
     },
     {
-      selector: 'div.modal:last-of-type',
-      html: `Select the cell lines 'HCC-827' and 'BT-20'. As data type, choose 'Relative Copy Number'. Finally click 'Add Single Score Column'`,
+      selector: '.modal.in .modal-content',
+      placement: 'right-start',
+      preAction: () => waitFor('.modal.in', 10000).then(() => wait(250)),
+      html: `Select the cell lines <i>'HCC-827'</i> and <i>'BT-20'</i>. As data type, choose <i>'Relative Copy Number'</i>. Finally click <i>'Add Single Score Column'</i>`,
       waitFor: () => waitFor('.lineup-engine header section[title^=BT]', Infinity).then(() => <'next'>'next')
     },
     {
       selector: '.lineup-engine header section[title^=BT], .lineup-engine header section[title^=HCC]',
+      placement: 'right',
+      preAction: () => waitFor('.lineup-engine header section[title^=BT]', 10000),
       html: `The copy number information for each selected cell line has been added as additional columns`,
     },
     {
       selector: '.lineup-engine > header',
+      placement: 'top',
       html: `The column headers can be used to sort and filter the list of genes based on any of the available data`,
     },
     {
       selector: '.lineup-engine header section[title^=BT] i[title^=Sort]',
+      placement: 'right',
       html: `For example, you can use this icon to sort all genes by their copy number in the cell line 'HCC-827'`,
-      waitFor: () => waitFor('.lu-row[data-meta=first][data-i="138"]', Infinity).then(() => <'next'>'next')
+      waitFor: () => waitFor(() => {
+        const r = document.querySelector<HTMLElement>('.lu-row[data-meta=first]');
+        if (!r) {
+          return null;
+        }
+        // has a string renderered EGFR entry
+        if (!Array.from(r.querySelectorAll<HTMLElement>('div[data-renderer="string"]')).some((d) => d.textContent === 'EGFR')) {
+          return null;
+        }
+        return r;
+      }, Infinity).then(() => <'next'>'next')
     },
     {
       selector: '.lu-row[data-meta=first]',
+      placement: 'bottom',
       html: `In order to obtain additional information about one or more genes, click on the respective line or use the checkboxes`,
       waitFor: () => waitFor('.viewWrapper .chooser:not(.hidden)', Infinity).then(() => <'next'>'next')
     },
     {
       selector: '.viewWrapper .chooser',
+      placement: 'left-start',
       html: `Various 'Detail Views', providing additional information, are available. For instance, clicking on 'Expression vs Copy Number' opens a scatter plot showing the relation of the two types of data.`,
     },
     {
