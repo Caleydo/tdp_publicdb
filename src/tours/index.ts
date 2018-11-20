7388e7b21d3876be5e0a153536ec523e29964459
Samuel Gratzl <sam@sgratzl.com>
2018-11-20 13:21:36 +0100

    disable placement

--- src/tours/index.ts
+++ src/tours/index.ts
@@ -16,7 +16,7 @@ export function create(): IStep[] {
     {
       selector: '.homeButton > a',
       html: `To start an analysis, click on the <i>'Home'</i> button. Subsequently, you can define the list of entities you want to work with`,
-      placement: 'right-start',
+      placement: null,
       preAction: () => {
         if (document.querySelector('.startMenu.open')) {
           click('.startMenu .closeButton');
@@ -28,14 +28,14 @@ export function create(): IStep[] {
     {
       selector: '.startMenu.open .speciesSelector .nav-tabs',
       html: `<p>You can choose between the three entity types <i>'Cell Lines'</i>, <i>'Genes'</i>, and <i>'Tissues'</i>.</p> <p>In this example we will work with a list of genes</p>`,
-      placement: 'right-start',
+      placement: null,
       postAction: () => click('#entityType_gene-entry-point')
     },
 
     {
       selector: '#entity_gene-entry-point .predefined-named-sets',
       html: `Of the available predefined gene sets, we open a list of known cancer genes, called <i>'Cancer Gene Census'</i>`,
-      placement: 'right-start',
+      placement: null,
       postAction: () => {
         return waitFor('#entity_gene-entry-point .predefined-named-sets a[title^="Name: Cancer Gene Census"]').then(click);
       },
@@ -43,20 +43,20 @@ export function create(): IStep[] {
     },
     {
       selector: '.lineup-engine.lineup-multi-engine',
-      placement: 'top',
+      placement: null,
       html: `The information is presented in a tabular format. Additionally to the gene ID, a set of columns containing some basic information is shown by default.`,
       preAction: () => waitFor('.lineup-engine.lineup-multi-engine', Infinity).then(() => wait(500))
     },
     {
       selector: '.lu-side-panel button.fa-plus',
       html: `Additional columns can be added using the plus sign.`,
-      placement: 'left',
+      placement: null,
       postAction: clickSelector
     },
     {
       selector: '.lu-search .lu-search-item',
       html: `First, we want to add a metadata column`,
-      placement: 'left',
+      placement: null,
       postAction: () => {
         click('.lu-search .lu-search-item');
         toggleClass('.lu-adder.once', 'once', false);
@@ -65,25 +65,25 @@ export function create(): IStep[] {
     {
       selector: '.modal.in select[name=column]',
       html: `Here we select <i>'Strand'</i> &hellip;`,
-      placement: 'right-start',
+      placement: null,
       preAction: () => waitFor('.modal.in select[name=column]').then(() => wait(250)),
       postAction: setValueAndTriggerSelector('strand')
     },
     {
       selector: '.modal.in .modal-footer button[type=submit]',
       html: `&hellip; and click <i>'Add Column'</i>`,
-      placement: 'right-start',
+      placement: null,
       postAction: clickSelector
     },
     {
       selector: '.lineup-engine header section[title=Strand]',
-      placement: 'right',
+      placement: null,
       html: `The strand information was added as a new column`,
       preAction: waitForSelector
     },
     {
       selector: '.lu-search .lu-search-group .lu-search-item',
-      placement: 'left',
+      placement: null,
       html: `Now, we want to add two columns containing the copy number information of two specific cell lines. To do so, we open the <i>'Cell Line Score'</i> dialog`,
       preAction: () => {
         click('.lu-side-panel button.fa-plus');
@@ -95,7 +95,7 @@ export function create(): IStep[] {
     },
     {
       selector: '.modal.in .form-group > .select3',
-      placement: 'right-start',
+      placement: null,
       preAction: () => waitFor('.modal.in').then(() => wait(250)),
       html: `We select the cell lines <i>'HCC-827'</i> and <i>'BT-20'</i>.`,
       postAction: () => {
@@ -104,7 +104,7 @@ export function create(): IStep[] {
     },
     {
       selector: '.modal.in .form-group > .select2',
-      placement: 'right-start',
+      placement: null,
       html: `As data type, we choose <i>'Relative Copy Number'</i>`,
       postAction: () => {
         setValueAndTrigger('.form-group > .select2 input.select2-search__field', 'Relative Copy Number', 'input');
@@ -114,29 +114,29 @@ export function create(): IStep[] {
     {
       selector: '.modal.in .modal-footer button[type=submit]',
       html: `Finally, click <i>'Add Single Score Column'</i>`,
-      placement: 'right-start',
+      placement: null,
       postAction: clickSelector
     },
     {
       selector: ['.lineup-engine header section[title^=BT], .lineup-engine header section[title^=HCC]'],
-      placement: 'right',
+      placement: null,
       preAction: () => waitFor('.lineup-engine header section[title^=BT]', 10000),
       html: `The copy number information for each selected cell line has been added as additional columns`,
     },
     {
       selector: '.lineup-engine > header',
-      placement: 'top',
+      placement: null,
       html: `The column headers can be used to sort and filter the list of genes based on any of the available data`,
     },
     {
       selector: '.lineup-engine header section[title^=HCC] i[title^=Sort]',
-      placement: 'right',
+      placement: null,
       html: `For example, you can use this icon to sort all genes by their copy number in the cell line <i>'HCC-827'</i>`,
       postAction: clickSelector
     },
     {
       selector: '.lu-row[data-meta=first]',
-      placement: 'bottom',
+      placement: null,
       html: `In order to obtain additional information about one or more genes, click on the respective line or use the checkboxes`,
       preAction: () => waitFor(() => {
         const r = document.querySelector<HTMLElement>('.lu-row[data-meta=first]');
@@ -153,13 +153,13 @@ export function create(): IStep[] {
     },
     {
       selector: '.viewWrapper .chooser:not(.hidden)',
-      placement: 'left-start',
+      placement: null,
       html: `Various 'Detail Views', providing additional information, are available.`,
       preAction: waitForSelector
     },
     {
       selector: '.viewWrapper .chooser button[data-viewid="celllinedb_expression_vs_copynumber"]',
-      placement: 'left-start',
+      placement: null,
       html: `For instance, clicking on <i>'Expression vs Copy Number'</i> opens a scatter plot showing the relation of the two types of data`,
       postAction: clickSelector
     },
