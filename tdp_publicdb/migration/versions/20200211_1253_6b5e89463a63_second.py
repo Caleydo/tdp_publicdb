eff604223805748397ff35c0e0eb38b8e927bc31
oltionchampari <oltion.champari@datavisyn.io>
2020-02-14 08:29:55 +0100

    Integrate alembic and create first empty revision Caleydo/tdp_publicdb#64

---
+++ tdp_publicdb/migration/versions/20200211_1253_6b5e89463a63_second.py
@@ -0,0 +1,30 @@
+"""Second
+
+Revision ID: 6b5e89463a63
+Revises:
+Create Date: 2020-02-11 12:53:39.869637
+
+"""
+from alembic import op
+import sqlalchemy as sa
+#
+# revision identifiers, used by Alembic.
+revision = '6b5e89463a63'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+  connection = op.get_bind()
+
+  connection.execute(
+        """
+
+        """
+    )
+  pass
+
+
+def downgrade():
+  pass
