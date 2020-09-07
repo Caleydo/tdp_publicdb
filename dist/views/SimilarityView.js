21ee2077edc9c5be458d01b4def44edc526c8126
oltionchampari <oltion.champari@datavisyn.io>
2020-09-07 14:38:18 +0200

    Merge tdp_genehopper into tdp_publicdb
    Caleydo/tdp_bi_bioinfodb#1134

---
+++ dist/views/SimilarityView.js
@@ -0,0 +1,124 @@
+/**
+ * Created by Samuel Gratzl on 29.01.2016.
+ */
+import { tsv } from 'd3';
+import { ARankingView, ColumnDescUtils } from 'tdp_core';
+import { FormElementType } from 'tdp_core';
+import { SpeciesUtils } from 'tdp_gene';
+import { IDTypeManager } from 'phovea_core';
+import { RestBaseUtils } from 'tdp_core';
+import { createSelectionDesc, createStackDesc } from 'lineupjs';
+const SELECT_ID = 'genehopper_selection';
+export class SimilarityView extends ARankingView {
+    constructor(context, selection, parent) {
+        super(context, selection, parent);
+        this.loader = null;
+        this.node.classList.add('genehopper_similarity');
+    }
+    getParameterFormDescs() {
+        return super.getParameterFormDescs().concat([
+            {
+                type: FormElementType.SELECT,
+                label: 'Show',
+                id: SELECT_ID,
+                options: {
+                    optionsData: []
+                }
+            }
+        ]);
+    }
+    get itemIDType() {
+        return this.idType;
+    }
+    async updateOptionsData() {
+        const ids = await this.resolveSelection();
+        const options = await SpeciesUtils.createOptions(ids, this.selection, this.idType);
+        const select = this.getParameterElement(SELECT_ID);
+        // backup entry and restore the selectedIndex by value afterwards again,
+        // because the position of the selected element might change
+        const bak = select.value;
+        select.updateOptionElements(options);
+        if (bak !== null) {
+            select.value = bak;
+        }
+    }
+    static convertData(data) {
+        return tsv.parse(data, (row) => {
+            const strings = ['name', 'ensgid', 'hom'];
+            Object.keys(row).forEach((name) => {
+                if (strings.indexOf(name) < 0) {
+                    row[name] = +row[name];
+                }
+            });
+            //(<any>row).id = KNOWN_GENES[(<any>row).name] || 22279;
+            return row;
+        });
+    }
+    async loadImpl() {
+        let gene = this.getParameter(SELECT_ID);
+        if (!gene) {
+            await this.updateOptionsData();
+            gene = this.getParameter(SELECT_ID);
+        }
+        const data = await RestBaseUtils.getTDPProxyData('genehopper_similar', { gene: gene.value }, 'text');
+        const rows = SimilarityView.convertData(data);
+        const columns = [ColumnDescUtils.stringCol('name', { label: 'Name' }),
+            ColumnDescUtils.stringCol('hom')
+        ];
+        const cols = ['bas', 'brs', 'cll', 'gbp', 'gcc', 'gdi', 'gmf', 'hgs', 'hor', 'ipr', 'pup', 'sin', 'swp', 'tis', 'vap'];
+        columns.push(...cols.map((d) => ColumnDescUtils.numberColFromArray(d, rows)));
+        const uids = await IDTypeManager.getInstance().resolveIdType(this.idType).map(rows.map((r) => r.ensgid));
+        rows.forEach((row, i) => {
+            row._id = uids[i];
+            row.id = row.ensgid;
+        });
+        return {
+            idType: this.idType,
+            columns,
+            rows
+        };
+    }
+    load() {
+        if (this.loader === null) {
+            this.loader = this.loadImpl();
+        }
+        return this.loader;
+    }
+    getColumnDescs(columns) {
+        return columns;
+    }
+    loadColumnDesc() {
+        return this.load();
+    }
+    loadRows() {
+        return this.load().then((desc) => desc.rows);
+    }
+    parameterChanged(name) {
+        super.parameterChanged(name);
+        this.updateImpl();
+    }
+    selectionChanged() {
+        super.selectionChanged();
+        this.updateImpl();
+    }
+    updateImpl() {
+        this.updateOptionsData().then(() => {
+            this.loader = null;
+            this.rebuild();
+        });
+    }
+    createInitialRanking(provider) {
+        const columns = provider.getColumns();
+        const ranking = provider.pushRanking();
+        ranking.push(provider.create(createSelectionDesc()));
+        provider.push(ranking, columns[0]);
+        const stack = provider.push(ranking, createStackDesc('Combined'));
+        columns.filter((d) => d.type === 'number').forEach((d) => {
+            const col = provider.create(d);
+            col.setWidth(80);
+            stack.push(col);
+        });
+        stack.sortByMe();
+    }
+}
+//# sourceMappingURL=SimilarityView.js.map
