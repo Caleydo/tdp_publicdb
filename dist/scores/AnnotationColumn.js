ec43527c21fc2fcc7e7da37c32fad4fb72d32b37
oltionchampari <oltion.champari@datavisyn.io>
2020-10-07 15:07:23 +0200

    Support multiple in annotation score dialog

--- dist/scores/AnnotationColumn.js
+++ dist/scores/AnnotationColumn.js
@@ -1,7 +1,7 @@
 import { FormDialog } from 'tdp_core';
 import { ABooleanScore } from './ABooleanScore';
 import { FormElementType } from 'tdp_core';
-import { AppContext } from 'phovea_core';
+import { AppContext, I18nextManager } from 'phovea_core';
 import { ScoreUtils } from './ScoreUtils';
 import { chooseDataSource } from '../common/config';
 /**
@@ -19,33 +19,33 @@ export class AnnotationColumn extends ABooleanScore {
     }
     static createAnnotationColumnScore(data, pluginDesc) {
         const { primary } = ScoreUtils.selectDataSources(pluginDesc);
-        return new AnnotationColumn(data, primary);
+        return data.map((d) => new AnnotationColumn(d, primary));
     }
     /**
      * builder function for building the parameters of the score
      * @returns {Promise<IAnnotationColumnParam>} a promise for the parameter
      */
     static async createAnnotationColumn(pluginDesc) {
-        const dialog = new FormDialog('Add Annotation Column', 'Add');
+        const dialog = new FormDialog(I18nextManager.getInstance().i18n.t('tdp:publicdb.addAnnotation'), I18nextManager.getInstance().i18n.t('tdp:publicdb.add'));
         const dataSource = chooseDataSource(pluginDesc);
         const data = await AppContext.getInstance().getAPIJSON(`/tdp/db/publicdb/${dataSource.base}_panel`);
-        const optionsData = data.map((item) => ({ name: item.id, value: item.id }));
+        const optionsData = data.map((item) => ({ text: item.id, id: item.id }));
         dialog.append({
-            type: FormElementType.SELECT,
+            type: FormElementType.SELECT2_MULTIPLE,
             label: 'Named Set',
-            id: 'panel',
+            id: 'panels',
             attributes: {
                 style: 'width:100%'
             },
             required: true,
             options: {
-                optionsData
+                data: optionsData
             }
         });
         return dialog.showAsPromise((r) => {
-            // returning the whole data object, since there it contains all needed for the score
-            const data = r.getElementValues();
-            return data;
+            var _a;
+            const panels = (_a = r.getElementValues()) === null || _a === void 0 ? void 0 : _a.panels.map(({ id }) => ({ panel: id }));
+            return panels;
         });
     }
 }
