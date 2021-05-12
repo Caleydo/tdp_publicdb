8d5f192828971cd665878285f32a442d77f27664
Holger Stitz <holger.stitz@datavisyn.io>
2021-05-12 15:58:16 +0200

    Update .d.ts files of .tsx files

--- dist/common/forms.d.ts
+++ dist/common/forms.d.ts
@@ -150,7 +150,7 @@ export declare const FORM_GENE_FILTER: {
             return: string;
             optionsData: () => Promise<{
                 name: string;
-                value: import("lineupjs").ICompareValue;
+                value: string | number;
             }[]>;
             options: {
                 placeholder: string;
