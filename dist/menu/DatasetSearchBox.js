5a4522a970a5580219582d79942343f08288c226
Holger Stitz <holger.stitz@datavisyn.io>
2021-08-30 13:01:22 +0200

    Use hstack for searchbox due to reduced card width
    Requires https://github.com/Caleydo/ordino/pull/330
    
    See https://github.com/Caleydo/tdp_bi_bioinfodb/issues/1283#issuecomment-908136565

--- dist/menu/DatasetSearchBox.js
+++ dist/menu/DatasetSearchBox.js
@@ -54,59 +54,57 @@ export function DatasetSearchBox({ placeholder, dataSource, onOpen, onSaveAsName
             type: dataSource.tableName
         }
     };
-    return (React.createElement("div", { className: "row ordino-dataset-searchbox" },
-        React.createElement("div", { className: "col-sm-10" },
-            React.createElement(AsyncPaginate, { onPaste: onPaste, placeholder: placeholder, noOptionsMessage: () => 'No results found', isMulti: true, loadOptions: loadOptions, inputValue: inputValue, value: items, onChange: setItems, onInputChange: setInputValue, formatOptionLabel: formatOptionLabel, hideSelectedOptions: true, getOptionLabel: (option) => option.text, getOptionValue: (option) => option.id, captureMenuScroll: false, additional: {
-                    page: 0 // page starts from index 0
-                }, components: { Input }, styles: {
-                    multiValue: (styles, { data }) => ({
-                        ...styles,
-                        border: `1px solid #CCC`,
-                        borderRadius: '3px'
-                    }),
-                    multiValueLabel: (styles, { data }) => ({
-                        ...styles,
-                        textDecoration: data.invalid ? 'line-through' : 'none',
-                        color: data.color,
-                        backgroundColor: 'white',
-                        order: 2,
-                        paddingLeft: '0',
-                        paddingRight: '6px'
-                    }),
-                    multiValueRemove: (styles, { data }) => ({
-                        ...styles,
-                        color: data.invalid ? 'red' : '#999',
-                        backgroundColor: 'white',
-                        order: 1,
-                        ':hover': {
-                            color: '#333',
-                            cursor: 'pointer'
-                        },
-                    }),
-                    placeholder: (provided) => ({
-                        ...provided,
-                        // disable placeholder mouse events
-                        pointerEvents: 'none',
-                        userSelect: 'none',
-                    }),
-                    input: (css) => ({
-                        ...css,
-                        //expand the Input Component div
-                        flex: '1 1 auto',
-                        // expand the Input Component child div
-                        '> div': {
-                            width: '100%'
-                        },
-                        // expand the Input Component input
-                        input: {
-                            width: '100% !important',
-                            textAlign: 'left'
-                        }
-                    })
-                } })),
-        React.createElement("div", { className: "col-sm-2" },
-            React.createElement("button", { className: "me-2 pt-1 pb-1 btn btn-secondary", disabled: !(validItems === null || validItems === void 0 ? void 0 : validItems.length), onClick: (event) => onOpen(event, searchResults) }, "Open"),
-            React.createElement("button", { className: "me-2 pt-1 pb-1 btn btn-outline-secondary", disabled: !(validItems === null || validItems === void 0 ? void 0 : validItems.length), onClick: () => onSaveAsNamedSet(validItems) }, "Save as set"))));
+    return (React.createElement("div", { className: "hstack gap-3 ordino-dataset-searchbox" },
+        React.createElement(AsyncPaginate, { className: "flex-fill", onPaste: onPaste, placeholder: placeholder, noOptionsMessage: () => 'No results found', isMulti: true, loadOptions: loadOptions, inputValue: inputValue, value: items, onChange: setItems, onInputChange: setInputValue, formatOptionLabel: formatOptionLabel, hideSelectedOptions: true, getOptionLabel: (option) => option.text, getOptionValue: (option) => option.id, captureMenuScroll: false, additional: {
+                page: 0 // page starts from index 0
+            }, components: { Input }, styles: {
+                multiValue: (styles, { data }) => ({
+                    ...styles,
+                    border: `1px solid #CCC`,
+                    borderRadius: '3px'
+                }),
+                multiValueLabel: (styles, { data }) => ({
+                    ...styles,
+                    textDecoration: data.invalid ? 'line-through' : 'none',
+                    color: data.color,
+                    backgroundColor: 'white',
+                    order: 2,
+                    paddingLeft: '0',
+                    paddingRight: '6px'
+                }),
+                multiValueRemove: (styles, { data }) => ({
+                    ...styles,
+                    color: data.invalid ? 'red' : '#999',
+                    backgroundColor: 'white',
+                    order: 1,
+                    ':hover': {
+                        color: '#333',
+                        cursor: 'pointer'
+                    },
+                }),
+                placeholder: (provided) => ({
+                    ...provided,
+                    // disable placeholder mouse events
+                    pointerEvents: 'none',
+                    userSelect: 'none',
+                }),
+                input: (css) => ({
+                    ...css,
+                    //expand the Input Component div
+                    flex: '1 1 auto',
+                    // expand the Input Component child div
+                    '> div': {
+                        width: '100%'
+                    },
+                    // expand the Input Component input
+                    input: {
+                        width: '100% !important',
+                        textAlign: 'left'
+                    }
+                })
+            } }),
+        React.createElement("button", { className: "px-1 btn btn-secondary", disabled: !(validItems === null || validItems === void 0 ? void 0 : validItems.length), onClick: (event) => onOpen(event, searchResults) }, "Open"),
+        React.createElement("button", { className: "px-1 btn btn-outline-secondary", disabled: !(validItems === null || validItems === void 0 ? void 0 : validItems.length), onClick: () => onSaveAsNamedSet(validItems) }, "Save as set")));
 }
 function Input(props) {
     const { onPaste } = props.selectProps;
