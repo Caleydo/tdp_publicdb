b67cfd468a6d53bf3a7a1ee4c12e3975ecbcd05c
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2021-09-13 09:23:19 +0200

    change col-sm-auto to col-sm-12

--- dist/tours/AddColumnToGeneListTour.js
+++ dist/tours/AddColumnToGeneListTour.js
@@ -180,7 +180,7 @@ export class AddColumnToGeneListTour {
                 preAction: () => TourUtils.waitFor('.modal.show .modal-content').then(() => TourUtils.wait(250))
             },
             {
-                selector: '.modal-body form > .col-sm-auto:nth-child(1) .row:nth-child(1) div:nth-child(1) select',
+                selector: '.modal-body form > .col-sm-12:nth-child(1) .row:nth-child(1) div:nth-child(1) select',
                 html: `<p>First, we need to define the subset of cell lines which we want to aggregate.</p>
         <p>For the filter attribute we select <i>'Tumor Type'</i> &hellip;</p>`,
                 placement: 'centered',
@@ -189,29 +189,29 @@ export class AddColumnToGeneListTour {
                 }
             },
             {
-                selector: '.modal-body form > .col-sm-auto:nth-child(1) .row:nth-child(1) .row:nth-child(1) > div:nth-child(2) .select2',
+                selector: '.modal-body form > .col-sm-12:nth-child(1) .row:nth-child(1) .row:nth-child(1) > div:nth-child(2) .select2',
                 html: `&hellip; and for the filter value we select <i>'breast carcinoma'</i>.`,
                 placement: 'centered',
-                preAction: () => TourUtils.waitFor('.modal-body form > .col-sm-auto:nth-child(1) .row:nth-child(1) .row:nth-child(1) > div:nth-child(2) .select2').then(() => TourUtils.wait(250)),
+                preAction: () => TourUtils.waitFor('.modal-body form > .col-sm-12:nth-child(1) .row:nth-child(1) .row:nth-child(1) > div:nth-child(2) .select2').then(() => TourUtils.wait(250)),
                 postAction: () => {
-                    TourUtils.setValueAndTrigger('.modal-body form > .col-sm-auto:nth-child(1) .row:nth-child(1) .row:nth-child(1) > div:nth-child(2) select', 'breast carcinoma', 'change');
+                    TourUtils.setValueAndTrigger('.modal-body form > .col-sm-12:nth-child(1) .row:nth-child(1) .row:nth-child(1) > div:nth-child(2) select', 'breast carcinoma', 'change');
                 }
             },
             {
-                selector: '.modal-body form > .col-sm-auto:nth-child(2) .row:nth-child(1) .select2',
+                selector: '.modal-body form > .col-sm-12:nth-child(2) .row:nth-child(1) .select2',
                 placement: 'centered',
                 html: `As data type, we choose <i>'Relative Copy Number'</i> &hellip;`,
                 postAction: () => {
-                    TourUtils.setValueAndTrigger('.modal-body form > .col-sm-auto:nth-child(2) select', 'copy_number-relativecopynumber', 'change');
+                    TourUtils.setValueAndTrigger('.modal-body form > .col-sm-12:nth-child(2) select', 'copy_number-relativecopynumber', 'change');
                 }
             },
             {
-                selector: '.modal-body form > .col-sm-auto:nth-child(3) select',
+                selector: '.modal-body form > .col-sm-12:nth-child(3) select',
                 placement: 'centered',
                 html: `<p>&hellip; and as type of aggregation we choose <i>'Average'</i></p>
         <p>FYI: other types of aggregation are, for instance, <i>'Min'</i>, <i>'Max'</i>, <i>'Median'</i>, <i>'Count'</i>, and <i>'Boxplot'</i>.`,
                 postAction: () => {
-                    TourUtils.setValueAndTrigger('.modal-body form > .col-sm-auto:nth-child(3) > select', 'avg', 'change');
+                    TourUtils.setValueAndTrigger('.modal-body form > .col-sm-12:nth-child(3) > select', 'avg', 'change');
                 }
             },
             {
