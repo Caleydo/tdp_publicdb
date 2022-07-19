fbc855a6eccf325182d2ee34df3a33181ca86d67
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2022-07-19 07:58:42 +0200

    add index.js

---
+++ cypress/integration/index.js
@@ -0,0 +1,4 @@
+export * from 'tdp_bi_bioinfodb/cypress/integration/long-running-score.spec';
+export * from './session_all_celllines.spec';
+export * from './session_all_genes.spec';
+export * from './session_all_tissues.spec';
