8271dec34b931dc61faafba43ba77e4a57d83cc8
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2022-07-25 16:47:34 +0200

    fix lint and update dist

--- src/views/InfoTable.ts
+++ src/views/InfoTable.ts
@@ -2,9 +2,7 @@
  * Created by Samuel Gratzl on 27.04.2016.
  */
 
-import { IViewContext, ISelection, IView, IDTypeManager } from 'tdp_core';
-import { AD3View } from 'tdp_core';
-import { RestBaseUtils } from 'tdp_core';
+import { IViewContext, ISelection, IView, IDTypeManager, AD3View, RestBaseUtils } from 'tdp_core';
 import { SpeciesUtils } from 'tdp_gene';
 import { Primitive, transpose as d3Transpose, Selection } from 'd3';
 import { IDataSourceConfig, cellline, tissue, gene } from '../common/config';
