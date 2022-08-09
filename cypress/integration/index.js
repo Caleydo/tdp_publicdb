1e07eb15809fc6790e788f93916005655e3c4dd2
Michael Peterseil <michael.peterseil@datavisyn.io>
2022-08-09 11:12:01 +0200

    Refactored cy related files for Cypress v10

--- cypress/integration/index.js
+++
@@ -1,6 +0,0 @@
-export * from './long-running-score.spec';
-export * from './session_all_genes.spec';
-export * from './session_all_celllines.spec';
-export * from './session_all_tissues.spec';
-
-export * from './ordino-video-test.spec';
