6488f23b3bf9ee29d33dbc08b4da4918fdd61b6f
anita-steiner <>
2022-08-10 08:52:45 +0000

    Prepare visyn_script changes

--- src/phovea_registry.ts
+++ src/phovea_registry.ts
@@ -4,16 +4,16 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 
-import {PluginRegistry} from 'tdp_core';
-import reg from './dist/phovea';
+import { PluginRegistry } from 'tdp_core';
+import reg from './phovea';
 
 /**
  * build a registry by registering all phovea modules
  */
-//other modules
-import 'tdp_gene/phovea_registry.js';
+// other modules
+import 'tdp_gene/dist/phovea_registry';
 /// #if include('ordino')
-import 'ordino/phovea_registry.js';
+import 'ordino/dist/phovea_registry';
 /// #endif
-//self
-PluginRegistry.getInstance().register('tdp_publicdb',reg);
+// self
+PluginRegistry.getInstance().register('tdp_publicdb', reg);
