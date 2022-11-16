ae5ffb69fe15f7317d2c195cf4bbdade38f0067d
Holger Stitz <holger.stitz@datavisyn.io>
2022-11-16 10:28:31 +0100

    fix: domain of gene signature scores
    Caleydo/tdp_bi_bioinfodb#1402

--- dist/scores/GeneSignatureScore.js
+++ dist/scores/GeneSignatureScore.js
@@ -25,7 +25,7 @@ export class GeneSignatureScore {
      */
     createDesc() {
         const label = this.options.description;
-        return ColumnDescUtils.numberCol('_gene_signature', -5, 5, { label });
+        return ColumnDescUtils.numberCol('_gene_signature', null, null, { label }); // null, null = infer min/max automatically from the data
     }
     /**
      * Computes the actual scores and returns a Promise of IScoreRow rows.
