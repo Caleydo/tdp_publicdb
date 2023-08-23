4299184c5a027a04b233fe299d9d5a78911face8
Holger Stitz <holger.stitz@datavisyn.io>
2023-08-23 11:15:08 +0200

    feat: parse JSON in engine settings to dict

--- tdp_publicdb/settings.py
+++ tdp_publicdb/settings.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+import contextlib
+import json
+
+from pydantic import BaseModel, validator
 from visyn_core import manager
 
 
@@ -6,7 +9,17 @@ class TDPPublicDBSettings(BaseModel):
     dburl: str = "postgresql://publicdb:publicdb@publicdb:5432/publicdb"
     statement_timeout: str = "'5min'"
     statement_timeout_query: str = "set statement_timeout to {}"
-    engine: dict = {"pool_pre_ping": True}
+    engine: dict[str, str] | None = {"pool_pre_ping": True}
+    poolclass: str | None = "QueuePool"
+
+    @validator("engine", pre=True)
+    def json_decode_headers(cls, v):  # NOQA N805
+        # Manually parse JSON strings if they are coming from the env via `VISYN_CORE__...='{"...": ...}'`.
+        # See https://github.com/pydantic/pydantic/issues/831 for details.
+        if isinstance(v, str):
+            with contextlib.suppress(ValueError):
+                return json.loads(v)
+        return v
 
 
 def get_settings() -> TDPPublicDBSettings:
